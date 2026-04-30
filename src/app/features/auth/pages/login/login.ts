82fd63b8e606e088f04fbee72b179420cabfb8ec
Mia Flores <miaflores.proyectosti@gmail.com>
2026-04-30 16:57:58 -0500

    feat: update login

--- src/app/features/auth/pages/login/login.ts
+++ src/app/features/auth/pages/login/login.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { AuthService } from '../../../../core/services/auth';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
   standalone: true,
+  imports: [FormsModule, NgIf],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
@@ -15,17 +15,36 @@ export class Login {
   email = '';
   password = '';
 
+  showPassword = false;
+  error = '';
+  loading = false;
+
   constructor(
-    private authService: AuthService, 
-    private router: Router) {}
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   login() {
+    this.error = '';
+
+    // VALIDACIONES (frontend)
+    if (!this.email || !this.password) {
+      this.error = 'Complete todos los campos';
+      return;
+    }
+
+    this.loading = true;
+
     this.authService.login(this.email, this.password).subscribe({
       next: () => {
+        this.loading = false;
         this.router.navigate(['/']);
       },
-      error: (error) => {
-        console.error('Login failed', error);
+      error: (err) => {
+        this.loading = false;
+
+        // mensaje backend o fallback
+        this.error = err?.error?.message || 'Error al iniciar sesión';
       }
     });
   }
